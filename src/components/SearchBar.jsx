6e29ad8b57ef34e7dbcb9ee2e9f05ab878bdaa18
Virginia Duca <virginiambd.bio@gmail.com>
2020-08-17 23:31:59 -0300

    requisito 2 codeclimate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import  changeFilterByName  from './actions/actionsFilter';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import changeFilterByName from './actions/actionsFilter';
 
 function filterForm() {
   return (
     <div>
-      <select data-testid='column-filter' placeholder="SELECT">
-{/*         <option value="">Coluna</option>
- */}        <option value='population'>Population</option>
+      <select data-testid="column-filter" placeholder="SELECT">
+        <option value="population">Population</option>
         <option value="orbital_period">Orbital Period</option>
         <option value="diameter">Diameter</option>
         <option value="rotation_period">Rotation Period</option>
         <option value="surface_water">Surface Water</option>
       </select>
-      <select data-testid='comparison-filter'>
-{/*         <option value="">Comparação</option>
- */}        <option value="maior_que">Maior que:</option>
+      <select data-testid="comparison-filter">
+        <option value="maior_que">Maior que:</option>
         <option value="menor_que">Menor que:</option>
         <option value="igual_a">Igual a:</option>
       </select>
-      <input type="number" data-testid='value-filter'/>
-      <button type="button" data-testid='button-filter'>Acionar Filtro</button>
+      <input type="number" data-testid="value-filter" />
+      <button type="button" data-testid="button-filter">Acionar Filtro</button>
     </div>
-  )
+  );
 }
 
 class SearchBar extends React.Component {
@@ -35,11 +34,10 @@ class SearchBar extends React.Component {
   }
 
   handleChange(event) {
-    const { changeFilterByName } = this.props
     this.setState({
-      inputText: event.target.value
-    })
-    changeFilterByName(event.target.value);
+      inputText: event.target.value,
+    });
+    this.props.changeFilterByName(event.target.value);
   }
 
   render() {
@@ -47,22 +45,26 @@ class SearchBar extends React.Component {
     return (
       <div>
         <form>
-          <label>Procurar</label>
-          <input 
-          placeholder="Name"
-          onChange={this.handleChange}
-          value={inputText}
-          data-testid='name-filter'
+          <label htmlFor="name-filter">Procurar</label>
+          <input
+            placeholder="Name"
+            onChange={this.handleChange}
+            value={inputText}
+            data-testid="name-filter"
           />
         </form>
         {filterForm()}
-      </div>        
-    )
+      </div>
+    );
   }
 }
 
 const mapDispatchToProps = (dispatch) => ({
   changeFilterByName: (nameInput) => dispatch(changeFilterByName(nameInput)),
 });
+
+SearchBar.propTypes = {
+  changeFilterByName: PropTypes.instanceOf(Object).isRequired,
+};
 
 export default connect(null, mapDispatchToProps)(SearchBar);
--- src/components/actions/actionsFilter.js
+++ src/components/actions/actionsFilter.js
@@ -1,7 +1,7 @@
-export const CHANGE_FILTERBYNAME = 'CHANGE_FILTERBYNAME'
+export const CHANGE_FILTERBYNAME = 'CHANGE_FILTERBYNAME';
 
 function changeFilterByName(nameInput) {
-  return { type: CHANGE_FILTERBYNAME,  nameInput}
-};
+  return { type: CHANGE_FILTERBYNAME, nameInput };
+}
 
 export default changeFilterByName;
